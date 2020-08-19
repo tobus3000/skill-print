820902c2ab7de7655c26cfb1c88c362a37259b39
Tobias Hildebrand <thildebrand@tobo.lan>
2020-08-19 20:52:51 +0200

    voice activate/deactive printer skill

--- __init__.py
+++ __init__.py
@@ -11,19 +11,20 @@ Print support Mycroft Skill
 class Print(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.printer_active = False
         self.print_dev = ""
         self.print_time = True
         self.print_all = False
         self.print_lf = False
         self.bucket_size = 50
         self.msg_bucket = []
-        self.error_state = False
 
     def initialize(self):
         self.add_event('speak', self.handler_speak)
         self.add_event('configuration.updated', self.handler_configuration_updated)
         self.load_configuration()
         self.register_entity_file('target.entity')
+        self.register_entity_file('action.entity')
 
     def stop(self):
         pass
@@ -37,6 +38,7 @@ class Print(MycroftSkill):
         return
 
     def load_configuration(self):
+        self.printer_active = self.settings.get('printeractive', False)
         self.print_dev = self.settings.get('printdev')
         self.print_all = self.settings.get('printall', False)
         self.print_lf = self.settings.get('printlf', False)
@@ -59,6 +61,16 @@ class Print(MycroftSkill):
     def bucket_get(self, message, amount):
         pass
 
+    def enable_printer(self):
+        self.printer_active = True
+        self.settings.set('printeractive', True)
+        return
+
+    def disable_printer(self):
+        self.printer_active = False
+        self.settings.set('printeractive', False)
+        return
+
 
     """
     Handle intents
@@ -78,6 +90,22 @@ class Print(MycroftSkill):
 ##        result = self.fetch_status(target)
 ##        self.log.debug(result)
 
+    @intent_handler('enabledisable.intent')
+    def handler_config(self, message):
+        self.log.debug("Running config handler....")
+        action = message.data.get('action')
+        if action is None:
+            self.speak("I did not understand. Please repeat.")
+            return
+        
+        if action == "enable" or action == "activate":
+            self.speak("Enabling printer.")
+            self.enable_printer()
+        elif action == "disable" or action == "deactivate":
+            self.speak("Disabling printer.")
+            self.disable_printer()
+
+
     def handler_speak(self, message):
         self.bucket_add(message)
         if self.print_all:
@@ -88,7 +116,7 @@ class Print(MycroftSkill):
         
     def print_out(self, target):
         self.log.debug("Printing...")
-        if self.print_time and self.error_state == False:
+        if self.print_time and self.printer_active:
             tz = pytz.timezone(self.location_timezone)
             cur_time_obj = datetime.now(tz)
             print_time = cur_time_obj.strftime('%d.%m.%Y %H:%M:%S %Z %z')
@@ -96,24 +124,24 @@ class Print(MycroftSkill):
             exit_code = os.system(cmd_ts)
             if exit_code != 0:
                 self.speak_dialog('error')
-                self.error_state = True
-
+                self.disable_printer()
+                
         #cmd_ts = 'echo "' + str(print_time) + ':" >> ' + self.print_dev
         #exit_code = os.system(cmd_ts)
-        if self.error_state == False: 
+        if self.printer_active: 
             cmd = 'echo "' + str(target) + '" >> ' + self.print_dev
             exit_code = os.system(cmd)  # returns the exit code in unix
             if exit_code != 0:
                 self.speak_dialog('error')
-                self.error_state = True
+                self.disable_printer()
 
             #self.log.debug("Returned value:" + str(exit_code))
-        if self.error_state == False and self.print_lf == True:
+        if self.printer_active and self.print_lf:
             cmd = 'echo "" >> ' + self.print_dev
             exit_code = os.system(cmd)
             if exit_code != 0:
                 self.speak_dialog('error')
-                self.error_state = True
+                self.disable_printer()
 
 
 
