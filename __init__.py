5e5a4278b596350213a874739cdf896363341c63
Tobias Hildebrand <thildebrand@tobo.lan>
2020-08-22 10:51:49 +0200

    testign

--- __init__.py
+++ __init__.py
@@ -90,17 +90,17 @@ class Print(MycroftSkill):
 
     """ Some checking to avoid shell command injection and to make sure the device is active. """
     def is_valid_printdev(self, printdev):
-        device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+        #device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
         df = subprocess.check_output("lsusb")
         devices = []
-        for i in df.split('\n'):
-            if i:
-                info = device_re.match(i)
-                if info:
-                    dinfo = info.groupdict()
-                    dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
-                    devices.append(dinfo)
-        self.debug.log(str(devices));
+        #for i in df.split('\n'):
+        #    if i:
+        #        info = device_re.match(i)
+        #        if info:
+        #            dinfo = info.groupdict()
+        #            dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
+        #            devices.append(dinfo)
+        self.debug.log(str(df));
         return True
 
     """
